cc02f484fed0fe6ca219d16288059bd3dad6032d
Gabriel <gabrielamarov10@gmail.com>
2026-01-06 14:53:24 +0000

    Replace String variables with enum classes

--- composeApp/src/commonMain/kotlin/com/exalted/exaltedapp/function/SmallHelpers.kt
+++ composeApp/src/commonMain/kotlin/com/exalted/exaltedapp/function/SmallHelpers.kt
@@ -1,19 +1,14 @@
 package com.exalted.exaltedapp.function
 
+import com.exalted.exaltedapp.data.Difficulty
+import com.exalted.exaltedapp.data.Priority
 import com.exalted.exaltedapp.data.ToDoItem
 import com.exalted.exaltedapp.data.progression.SkillType
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-fun priorityWeight(priority: String): Int = when (priority) {
-    "High" -> 3
-    "Medium" -> 2
-    "Low" -> 1
-    else -> 0
-}
-
 @OptIn(ExperimentalUuidApi::class)
-fun makeToDoItem(entry: String, description: String, priority: String, difficulty: String, skill: SkillType): ToDoItem {
+fun makeToDoItem(entry: String, description: String, priority: Priority, difficulty: Difficulty, skill: SkillType): ToDoItem {
     val completed = false
     val id = Uuid.random().toString();
 
