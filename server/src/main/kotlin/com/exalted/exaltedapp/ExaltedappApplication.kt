920b47be74053004d8ba556449391b006a3d6ee3
Gabriel Amarov <github@mail.amarov.dev>
2026-01-09 14:30:43 +0000

    Add Spring Boot Backend module

---
+++ server/src/main/kotlin/com/exalted/exaltedapp/ExaltedappApplication.kt
@@ -0,0 +1,11 @@
+package com.exalted.exaltedapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExaltedappApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExaltedappApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/exalted/exaltedapp/ExaltedappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exalted.exaltedapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExaltedappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
