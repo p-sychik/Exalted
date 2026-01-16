b6f88381e3cebccf79cf3918b9439fe83b7fd614
Gabriel Amarov <github@mail.amarov.dev>
2026-01-16 15:11:25 +0000

    Improve Character page look And fix LazyColumn

--- composeApp/src/commonMain/kotlin/com/exalted/exaltedapp/function/Character.kt
+++ composeApp/src/commonMain/kotlin/com/exalted/exaltedapp/function/Character.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -11,6 +12,7 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,7 +29,7 @@ import kotlin.enums.enumEntries
 
 @Composable
 fun Character(modifier: Modifier, user : User) {
-    Column {
+    Column(modifier = modifier.fillMaxSize()) {
         StyledCard {
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -65,19 +67,27 @@ fun Character(modifier: Modifier, user : User) {
                     LinearProgressIndicator(
                         progress = { user.mainLevel.xp.toFloat() / user.mainLevel.xpToNext.toFloat() },
                         modifier = Modifier.fillMaxWidth().height(20.dp).padding(top = 8.dp),
+                        trackColor = MaterialTheme.colorScheme.background,
                     )
                 }
             }
         }
+        val listState = rememberLazyListState()
+
+        val skillList = remember(user.skills) {
+            user.skills.entries.toList()
+        }
+
         StyledCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 8.dp)
         ) {
             LazyColumn(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                state = listState
             ) {
-                items(user.skills.entries.toList()) { (skillType, skill) ->
+                items(skillList) { (skillType, skill) ->
 
                     Column(
                         modifier = Modifier.padding(all = 20.dp),
@@ -97,6 +107,7 @@ fun Character(modifier: Modifier, user : User) {
                         LinearProgressIndicator(
                             progress = { progress },
                             modifier = Modifier.fillMaxWidth().height(20.dp).padding(top = 8.dp),
+                            trackColor = MaterialTheme.colorScheme.background,
                         )
                     }
                     HorizontalDivider(
